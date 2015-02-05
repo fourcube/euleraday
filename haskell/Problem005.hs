0427b0caaa8580f80ed8c4965693a7015d366260
Chris Grieger <chris@scolp.de>
2015-02-05 09:46:10 +0100

    Cleanup

--- haskell/Problem005.hs
+++ haskell/Problem005.hs
@@ -14,8 +14,8 @@ module Problem005 where
   pow_tuple :: (Int, Int) -> Int
   pow_tuple (a, b) = a ^ b
 
-  group_factors :: [[Int]] -> [Int]
-  group_factors nested_factors = do
+  base_exponent_pairs :: [[Int]] -> [(Int, Int)]
+  base_exponent_pairs nested_factors = do
     -- transform [[1], [2], [3], [2,2], [5], [3,2], [7], [2,2,2] ...]
     let factor_groups = group $ concat $ map sort nested_factors
     -- into [[1], [2], [3], [2,2], [5], [2], [3], [7], [2,2,2], ...]
@@ -24,7 +24,11 @@ module Problem005 where
     let factor_count = map length factor_groups
 
     -- turn into a list of naturally sorted, unique (base, exponent) pairs
-    let factors = sort $ nub $ zip factor_type factor_count
+    sort $ nub $ zip factor_type factor_count
+
+  group_factors :: [[Int]] -> [Int]
+  group_factors nested_factors = do
+    let factors = base_exponent_pairs nested_factors
     -- group them into sublists by base, take the last entry, calculate base ^ exp
     -- this takes the greatest power of each prime and then multiplies it
     map pow_tuple $ map last $ groupBy (\a b -> fst a == fst b) $ factors
@@ -35,3 +39,6 @@ module Problem005 where
     -- Compute the prime factorization of each number in ints,
     -- and multiply the greatest power of each prime together
     foldl (*) 1 $ group_factors $ map prime_factors ints
+
+
+--filter (\x -> (length x) == 1 && (head x) > 1)  $ map nub $ (map . map) snd $ map base_exponent_pairs $ (map . map) prime_factors [[36],[20]]
